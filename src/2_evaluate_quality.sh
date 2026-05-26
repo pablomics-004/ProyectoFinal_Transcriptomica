5a333b2e63d086437eefc49e557cf1355f8df1a4
pablomics-004 <pablosm@lcg.unam.mx>
2026-05-26 02:23:26 -0600

    Evaluación de calidad de las lecturas crudas

--- src/2_evaluate_quality.sh
+++ src/2_evaluate_quality.sh
@@ -3,16 +3,11 @@
 set -euo pipefail # Si un comando falla, el script se detiene
 shopt -s nullglob # Evita errores si no se encuentran archivos coincidentes
 
-# Activando ambiente de conda para fastqc
-source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate multiqc
-
 # Directorios de trabajo
-DATA="./data"
-FASTQ="$DATA/fastq_raw"
+FASTQ="./data/fastq"
 
-mkdir -p "$FASTQ" ./logs/
+mkdir -p "${FASTQ}_raw" ./logs/
 
 # Evaluación de calidad de las lecturas utilizando fastqc en paralelo
-nohup fastqc "$FASTQ"/*_1.fastq -o "${FASTQ}c_1_raw" > ./logs/fastqc_1_explore.log 2>&1 &
-nohup fastqc "$FASTQ"/*_2.fastq -o "${FASTQ}c_2_raw" > ./logs/fastqc_2_explore.log 2>&1 &
+nohup fastqc "${FASTQ}_raw"/*_1.fastq -o "${FASTQ}c_1_raw" > ./logs/fastqc_1_raw_explore.log 2>&1 &
+nohup fastqc "${FASTQ}_raw"/*_2.fastq -o "${FASTQ}c_2_raw" > ./logs/fastqc_2_raw_explore.log 2>&1 &
