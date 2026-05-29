8083f5573812aeeb43800d9341d6688ec7f6fe36
pablomics-004 <pablosm@lcg.unam.mx>
2026-05-28 20:12:02 -0600

    Aceleración del proceso por reducción de paralelización

--- src/3_cleaning.sh
+++ src/3_cleaning.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 shopt -s nullglob
 
-# # Cambio interno de ambientes de conda
-# source "$(conda info --base)/etc/profile.d/conda.sh"
-# conda activate /home/pablosm/.conda/envs/mi_bioinfo
+# Cambio interno de ambientes de conda
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate /home/pablosm/.conda/envs/mi_bioinfo
 
 # Directorios de trabajo
 DATA="./data"
@@ -32,11 +32,11 @@ mkdir -p \
     ./logs
 
 # Configuración de hilos
-threads_fastp=12
+threads_fastp=8
 threads_fastqc=4
 
 # Número de procesos fastp en paralelo
-wks=2
+wks=1
 
 # Número de bases a remover al inicio
 erase_bp=13
@@ -67,8 +67,8 @@ for f in "${files[@]}"; do
     fastp \
         -i "${FASTQ}/${base}_1.fastq" \
         -I "${FASTQ}/${base}_2.fastq" \
-        -o "$CLEAN/${base}_clean_1.fastq" \
-        -O "$CLEAN/${base}_clean_2.fastq" \
+        -o "$CLEAN/${base}_clean_1.fastq.gz" \
+        -O "$CLEAN/${base}_clean_2.fastq.gz" \
         -w "$threads_fastp" \
         --trim_front1 "$erase_bp" \
         --trim_front2 "$erase_bp" \
@@ -86,11 +86,12 @@ if [[ ${#pids[@]} -gt 0 ]]; then
 fi
 
 echo "========== FASTQC SOBRE READS LIMPIOS =========="
+conda activate base
 
-clean_files_1=("$CLEAN"/*_clean_1.fastq)
+clean_files_1=("$CLEAN"/*_clean_1.fastq.gz)
 
 for f1 in "${clean_files_1[@]}"; do
-    base=$(basename "$f1" _clean_1.fastq)
+    base=$(basename "$f1" _clean_1.fastq.gz)
     f2="$CLEAN/${base}_clean_2.fastq"
 
     echo "Evaluando calidad de muestra limpia: $base"
@@ -111,6 +112,7 @@ for f1 in "${clean_files_1[@]}"; do
 done
 
 echo "========== MULTIQC =========="
+conda activate /home/pablosm/.conda/envs/mi_bioinfo
 
 multiqc "$DATA" -o "$MULTIQC_DIR"
 
